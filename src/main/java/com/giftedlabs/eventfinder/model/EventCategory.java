5723c729a50c293a587bd772b03258c8a164c7d5
Julius <juliussowah@amalitech.com>
2025-03-11 09:03:53 +0000

    Implemented the DTO classes

---
+++ src/main/java/com/giftedlabs/eventfinder/model/EventCategory.java
@@ -0,0 +1,19 @@
+package com.giftedlabs.eventfinder.model;
+
+public enum EventCategory {
+    CONFERENCE,
+    WORKSHOP,
+    SEMINAR,
+    CONCERT,
+    WEDDING,
+    FUNERAL,
+    PARTY,
+    CHURCH,
+    EXHIBITION,
+    SPORTS,
+    TECHNOLOGY,
+    ARTS,
+    FOOD,
+    NETWORKING,
+    OTHER
+}
